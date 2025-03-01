9851426b9455be769ab0c3f87963572fea2a8819
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2025-03-01 14:06:23 +0000

    feat: add website sections to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import CommunityFeatures from "@/components/sections/community-features";
+import EducationalCoverage from "@/components/sections/educational-coverage";
+import Footer from "@/components/sections/footer";
+import JoinUs from "@/components/sections/join-us";
+import PlatformBenefits from "@/components/sections/platform-benefits";
+import PlatformFeatures from "@/components/sections/platform-features";
+
 export default function Home() {
-  return <div className={"box"}></div>;
+  return (
+    <div>
+      <PlatformFeatures />
+      <EducationalCoverage />
+      <PlatformBenefits />
+      <CommunityFeatures />
+      <JoinUs />
+      <Footer />
+    </div>
+  );
 }
