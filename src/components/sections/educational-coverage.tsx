7e24a2c5988def2d248f670c020d6547eb342d7a
Abdellah M.El Idrissi <abdellahmoumenelidrissi@gmail.com>
2025-03-02 15:49:13 +0000

    feat: add multilanguage support to sections (#10)
    * feat: add en & ar messages for landing page sections
    
    * update: prevent scrolling to top when switching lang
    
    * update: header navigation links to the correct href
    
    * feat: add Multilanguage support to CommunityFeatures
    
    * feat: add Multilanguage support to EducationalCoverage
    
    * feat: add Multilanguage support to JoinUs
    
    * feat: add Multilanguage support to PlatformBenfits
    
    * feat: add Multilanguage support to PlatformFeatures
    
    * feat: add Multilanguage support to Footer
    
    * update(ui): hero subtitle to a max width of 41rem
    
    * chore: link build env port to 4000

--- src/components/sections/educational-coverage.tsx
+++ src/components/sections/educational-coverage.tsx
@@ -1,14 +1,16 @@
 import { BookOpenText, GraduationCap, School, Shapes } from "lucide-react";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function EducationalCoverage() {
+  const t = useTranslations("educationalCoverage");
+  const local = useLocale();
+
   return (
-    <div className="py-[50px] bg-[#f9fafb]">
+    <div className="py-[50px] bg-[#f9fafb]" id="education">
       <div className="box">
         <div className="text-center space-y-3">
-          <h2 className="text-3xl font-bold">Educational Coverage</h2>
-          <p className="text-neutral-500">
-            Complete coverage across all the educational levels
-          </p>
+          <h2 className="text-3xl font-bold">{t("title")}</h2>
+          <p className="text-neutral-500">{t("subtitle")}</p>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 md:flex-row gap-8 gap-y-9 mt-14 ">
@@ -18,24 +20,38 @@ export default function EducationalCoverage() {
             </div>
             <div className=" border-b border-neutral-200 pb-5 mt-5 ">
               <h3 className="text-xl font-semibold leading-none tracking-tight ">
-                Primary School
+                {t("levels.primary.title")}
               </h3>
               <div className="text-white text-center w-fit text-[11px] mt-3 leading-none font-normal rounded-sm px-2 bg-primary py-[5px] ">
-                Grades 1-6
+                {t("levels.primary.grades")}
               </div>
             </div>
 
             <div className="mt-5 space-y-3">
-              <p className="text-neutral-500 text-sm">Mathematics</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.primary.subjects.subject1")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Arabic Language</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.primary.subjects.subject2")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">French Language</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.primary.subjects.subject3")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Islamic Education</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.primary.subjects.subject4")}
+              </p>
 
               <p className="text-primary text-sm">
-                <span className="font-medium italic">+ 8 more subjects</span>
+                <span
+                  className={`italic ${
+                    local === "en" ? "font-meduim" : "font-semibold"
+                  }`}
+                >
+                  {t("levels.primary.additional")}
+                </span>
               </p>
             </div>
           </div>
@@ -46,24 +62,38 @@ export default function EducationalCoverage() {
             </div>
             <div className=" border-b border-neutral-200 pb-5 mt-5 ">
               <h3 className="text-xl font-semibold leading-none tracking-tight ">
-                Middle School
+                {t("levels.middle.title")}
               </h3>
               <div className="text-white w-fit text-center text-[11px] mt-3 leading-none font-normal rounded-sm px-2 bg-primary py-[5px] ">
-                Grades 7-9
+                {t("levels.middle.grades")}
               </div>
             </div>
 
             <div className="mt-5 space-y-3">
-              <p className="text-neutral-500 text-sm">Mathematics</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.middle.subjects.subject1")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Physics & Chemistry</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.middle.subjects.subject2")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Arabic Language</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.middle.subjects.subject3")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">English Language</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.middle.subjects.subject4")}
+              </p>
 
               <p className="text-primary text-sm">
-                <span className="font-medium italic">+ 10 more subjects</span>
+                <span
+                  className={`italic ${
+                    local === "en" ? "font-meduim" : "font-semibold"
+                  }`}
+                >
+                  {t("levels.middle.additional")}
+                </span>
               </p>
             </div>
           </div>
@@ -74,24 +104,38 @@ export default function EducationalCoverage() {
             </div>
             <div className=" border-b border-neutral-200 pb-5 mt-5 ">
               <h3 className="text-xl font-semibold leading-none tracking-tight ">
-                High School
+                {t("levels.high.title")}
               </h3>
               <div className="text-white w-fit text-center text-[11px] mt-3 leading-none font-normal rounded-sm bg-primary px-2 py-[5px] ">
-                Grades 10-12
+                {t("levels.high.grades")}
               </div>
             </div>
 
             <div className="mt-5 space-y-3">
-              <p className="text-neutral-500 text-sm">Science Tracks</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.high.subjects.subject1")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Literary Tracks</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.high.subjects.subject2")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Technical Tracks</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.high.subjects.subject3")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Economics Tracks</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.high.subjects.subject4")}
+              </p>
 
               <p className="text-primary text-sm">
-                <span className="font-medium italic">+ 5 specializations</span>
+                <span
+                  className={`italic ${
+                    local === "en" ? "font-meduim" : "font-semibold"
+                  }`}
+                >
+                  {t("levels.high.additional")}
+                </span>
               </p>
             </div>
           </div>
@@ -103,24 +147,36 @@ export default function EducationalCoverage() {
 
             <div className=" border-b border-neutral-200 pb-5 mt-5 ">
               <h3 className="text-xl font-semibold leading-none tracking-tight ">
-                Special Subjects
+                {t("levels.additional.title")}
               </h3>
               <div className="text-white w-fit text-center text-[11px] mt-3 leading-none font-normal rounded-sm bg-primary px-2 py-[5px] ">
-                Additional Coverage
+                {t("levels.additional.grades")}
               </div>
             </div>
             <div className="mt-5 space-y-3">
-              <p className="text-neutral-500 text-sm">Computer Science</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.additional.subjects.subject1")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Art Education</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.additional.subjects.subject2")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Languages</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.additional.subjects.subject3")}
+              </p>
 
-              <p className="text-neutral-500 text-sm">Physical Education</p>
+              <p className="text-neutral-500 text-sm">
+                {t("levels.additional.subjects.subject4")}
+              </p>
 
               <p className="text-primary text-sm">
-                <span className="font-medium italic">
-                  + 7 more specialized areas
+                <span
+                  className={`italic ${
+                    local === "en" ? "font-meduim" : "font-semibold"
+                  }`}
+                >
+                  {t("levels.additional.additional")}
                 </span>
               </p>
             </div>
