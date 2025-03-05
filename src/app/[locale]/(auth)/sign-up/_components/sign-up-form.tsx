b27a3d3dc4d51cfa7f4ea3ce609ab2fdb7a5196b
Fadad Oussama <fadadoussama7@gmail.com>
2025-03-05 18:04:39 +0100

    feat: integrate auth page (#14)
    * chore: install and integrate prettier-plugin-tailwindcss
    
    * feat: add card, input, and label components from shadcn
    
    * feat: update join btn's to redirect to sign-up page
    
    * feat: add auth route that includes sign up and sign in pages

---
+++ src/app/[locale]/(auth)/sign-up/_components/sign-up-form.tsx
@@ -0,0 +1,76 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function SignUpForm() {
+  return (
+    <form className="flex flex-col gap-6">
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold capitalize">sign up</h1>
+        <p className="text-balance text-sm text-muted-foreground">
+          Choose your preferred sign up method
+        </p>
+      </div>
+
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+
+        <div className="grid gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+
+        <Button type="submit" className="w-full">
+          Sign up
+        </Button>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+
+        <Button
+          variant="outline"
+          className="flex w-full flex-nowrap items-center justify-center gap-2"
+        >
+          <span className="size-4">
+            <svg viewBox="0 0 128 128">
+              <path
+                fill="#fff"
+                d="M44.59 4.21a63.28 63.28 0 004.33 120.9 67.6 67.6 0 0032.36.35 57.13 57.13 0 0025.9-13.46 57.44 57.44 0 0016-26.26 74.33 74.33 0 001.61-33.58H65.27v24.69h34.47a29.72 29.72 0 01-12.66 19.52 36.16 36.16 0 01-13.93 5.5 41.29 41.29 0 01-15.1 0A37.16 37.16 0 0144 95.74a39.3 39.3 0 01-14.5-19.42 38.31 38.31 0 010-24.63 39.25 39.25 0 019.18-14.91A37.17 37.17 0 0176.13 27a34.28 34.28 0 0113.64 8q5.83-5.8 11.64-11.63c2-2.09 4.18-4.08 6.15-6.22A61.22 61.22 0 0087.2 4.59a64 64 0 00-42.61-.38z"
+              ></path>
+              <path
+                fill="#e33629"
+                d="M44.59 4.21a64 64 0 0142.61.37 61.22 61.22 0 0120.35 12.62c-2 2.14-4.11 4.14-6.15 6.22Q95.58 29.23 89.77 35a34.28 34.28 0 00-13.64-8 37.17 37.17 0 00-37.46 9.74 39.25 39.25 0 00-9.18 14.91L8.76 35.6A63.53 63.53 0 0144.59 4.21z"
+              ></path>
+              <path
+                fill="#f8bd00"
+                d="M3.26 51.5a62.93 62.93 0 015.5-15.9l20.73 16.09a38.31 38.31 0 000 24.63q-10.36 8-20.73 16.08a63.33 63.33 0 01-5.5-40.9z"
+              ></path>
+              <path
+                fill="#587dbd"
+                d="M65.27 52.15h59.52a74.33 74.33 0 01-1.61 33.58 57.44 57.44 0 01-16 26.26c-6.69-5.22-13.41-10.4-20.1-15.62a29.72 29.72 0 0012.66-19.54H65.27c-.01-8.22 0-16.45 0-24.68z"
+              ></path>
+              <path
+                fill="#319f43"
+                d="M8.75 92.4q10.37-8 20.73-16.08A39.3 39.3 0 0044 95.74a37.16 37.16 0 0014.08 6.08 41.29 41.29 0 0015.1 0 36.16 36.16 0 0013.93-5.5c6.69 5.22 13.41 10.4 20.1 15.62a57.13 57.13 0 01-25.9 13.47 67.6 67.6 0 01-32.36-.35 63 63 0 01-23-11.59A63.73 63.73 0 018.75 92.4z"
+              ></path>
+            </svg>
+          </span>
+          Sign up with GitHub
+        </Button>
+      </div>
+
+      <div className="text-center text-sm">
+        Already have an account ?{" "}
+        <a href="#" className="underline underline-offset-4">
+          Sign In
+        </a>
+      </div>
+    </form>
+  );
+}
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
 import { MoveRight, MoveLeft } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
 
 import { useLocale, useTranslations } from "next-intl";
@@ -17,7 +17,7 @@ export const Hero = () => {
       locale === "en"
         ? ["Connect", "Engage", "Collaborate"]
         : ["يتواصلون", "ينخرطون", "يتعاونون"],
-    [locale]
+    [locale],
   );
 
   const t = useTranslations("hero");
@@ -34,39 +34,39 @@ export const Hero = () => {
   }, [titleNumber, titles]);
 
   return (
-    <div className="relative overflow-hidden w-full hero-height">
+    <div className="hero-height relative w-full overflow-hidden">
       <div className="absolute inset-0 -z-10">
         <Image className="object-cover" src={"/hero-bg.png"} fill alt="" />
         <div className="absolute inset-0 bg-white/50"></div>
       </div>
 
-      <div className="container mx-auto flex justify-center items-center h-full">
-        <div className="hero-page flex gap-8 py-40 items-center justify-center flex-col md:px-0 px-5">
+      <div className="container mx-auto flex h-full items-center justify-center">
+        <div className="hero-page flex flex-col items-center justify-center gap-8 px-5 py-40 md:px-0">
           <div className="relative overflow-hidden">
             <Link href="#features" className="navLink">
               <Button
                 variant="outline"
                 size="sm"
-                className="gap-4 bg-white border-neutral-100 sm:text-sm text-xs"
+                className="gap-4 border-neutral-100 bg-white text-xs sm:text-sm"
               >
                 {t("discoverBtn")}{" "}
                 {locale === "en" ? (
-                  <MoveRight className="w-4 h-4" />
+                  <MoveRight className="h-4 w-4" />
                 ) : (
-                  <MoveLeft className="w-4 h-4" />
+                  <MoveLeft className="h-4 w-4" />
                 )}
               </Button>
             </Link>
           </div>
 
-          <div className="flex gap-4 flex-col">
-            <h1 className="text-5xl md:text-6xl lg:text-7xl max-w-4xl mx-auto tracking-tighter font-normal text-center">
+          <div className="flex flex-col gap-4">
+            <h1 className="mx-auto max-w-4xl text-center text-5xl font-normal tracking-tighter md:text-6xl lg:text-7xl">
               <span className="text-spektr-cyan-50">{t("title")} </span>
               <span
-                className={`relative overflow-hidden flex w-full justify-center text-center ${
+                className={`relative flex w-full justify-center overflow-hidden text-center ${
                   locale === "en"
-                    ? "min-h-[52px] md:min-h-[66px] lg:min-h-[78px] md:pt-1"
-                    : "min-h-[68px] md:min-h-[90px] lg:min-h-[120px] pt-2 md:pt-4"
+                    ? "min-h-[52px] md:min-h-[66px] md:pt-1 lg:min-h-[78px]"
+                    : "min-h-[68px] pt-2 md:min-h-[90px] md:pt-4 lg:min-h-[120px]"
                 }`}
               >
                 &nbsp;
@@ -94,41 +94,45 @@ export const Hero = () => {
               </span>
             </h1>
 
-            <p className="text-base md:text-lg lg:text-xl leading-relaxed tracking-tight text-neutral-800 max-w-[41rem] mt-2 mx-auto font-normal text-center">
+            <p className="mx-auto mt-2 max-w-[41rem] text-center text-base font-normal leading-relaxed tracking-tight text-neutral-800 md:text-lg lg:text-xl">
               {t("subtitle")}
             </p>
           </div>
 
-          <div className="flex flex-row justify-center items-center gap-3 flex-wrap">
-            <Button size="lg" className="gap-3 border-primary">
-              {t("teacherBtn")}{" "}
-              <span className="w-4 h-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M12 14V16C8.68629 16 6 18.6863 6 22H4C4 17.5817 7.58172 14 12 14ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11ZM18 21.5L15.0611 23.0451L15.6224 19.7725L13.2447 17.4549L16.5305 16.9775L18 14L19.4695 16.9775L22.7553 17.4549L20.3776 19.7725L20.9389 23.0451L18 21.5Z"></path>
-                </svg>
-              </span>
-            </Button>
+          <div className="flex flex-row flex-wrap items-center justify-center gap-3">
+            <Link href="/sign-up?mode=teacher">
+              <Button size="lg" className="gap-3 border-primary">
+                {t("teacherBtn")}{" "}
+                <span className="h-4 w-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="M12 14V16C8.68629 16 6 18.6863 6 22H4C4 17.5817 7.58172 14 12 14ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11ZM18 21.5L15.0611 23.0451L15.6224 19.7725L13.2447 17.4549L16.5305 16.9775L18 14L19.4695 16.9775L22.7553 17.4549L20.3776 19.7725L20.9389 23.0451L18 21.5Z"></path>
+                  </svg>
+                </span>
+              </Button>
+            </Link>
 
-            <Button
-              size="lg"
-              className="gap-3 bg-white border-neutral-100"
-              variant="outline"
-            >
-              {t("studentBtn")}{" "}
-              <span className="w-4 h-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M14 14.252V16.3414C13.3744 16.1203 12.7013 16 12 16C8.68629 16 6 18.6863 6 22H4C4 17.5817 7.58172 14 12 14C12.6906 14 13.3608 14.0875 14 14.252ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11ZM17.7929 19.9142L21.3284 16.3787L22.7426 17.7929L17.7929 22.7426L14.2574 19.2071L15.6716 17.7929L17.7929 19.9142Z"></path>
-                </svg>
-              </span>
-            </Button>
+            <Link href="/sign-up?mode=student">
+              <Button
+                size="lg"
+                className="gap-3 border-neutral-100 bg-white"
+                variant="outline"
+              >
+                {t("studentBtn")}{" "}
+                <span className="h-4 w-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="M14 14.252V16.3414C13.3744 16.1203 12.7013 16 12 16C8.68629 16 6 18.6863 6 22H4C4 17.5817 7.58172 14 12 14C12.6906 14 13.3608 14.0875 14 14.252ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11ZM17.7929 19.9142L21.3284 16.3787L22.7426 17.7929L17.7929 22.7426L14.2574 19.2071L15.6716 17.7929L17.7929 19.9142Z"></path>
+                  </svg>
+                </span>
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
