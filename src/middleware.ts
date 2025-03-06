fea6c60032e5be68799c82c2609eab1aeb6a2f69
Abdellah M.El Idrissi <abdellahmoumenelidrissi@gmail.com>
2025-03-06 17:38:56 +0000

    feat: setup middleware with clerk & next-intl

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,64 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
 import { NextResponse } from "next/server";
+import { routing } from "./i18n/routing";
 
 const intlMiddleware = createMiddleware(routing);
-const isPublicRoute = createRouteMatcher([
-  "(en|ar)/",
-  "/(en|ar)/sign-in(.*)",
-  "/(en|ar)/sign-up(.*)",
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/:locale/dashboard(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+const isAuthPage = createRouteMatcher([
+  "/sign-in(.*)",
+  "/:locale/sign-in(.*)",
+  "/sign-up(.*)",
+  "/:locale/sign-up(.*)",
+]);
+
+export default function middleware(
+  request: NextRequest,
+  event: NextFetchEvent,
+) {
+  // Run Clerk middleware only when it's necessary
+  if (isAuthPage(request) || isProtectedRoute(request)) {
+    return clerkMiddleware(async (auth, req) => {
+      if (isProtectedRoute(req)) {
+        const locale =
+          req.nextUrl.pathname.match(/(\/.*)\/dashboard/)?.at(1) ?? "";
+
+        const signInUrl = new URL(`${locale}/sign-in`, req.url);
+
+        await auth.protect({
+          // `unauthenticatedUrl` is needed to avoid error: "Unable to find `next-intl` locale because the middleware didn't run on this request"
+          unauthenticatedUrl: signInUrl.toString(),
+        });
+      }
+
+      return intlMiddleware(req);
+    })(request, event);
   }
 
-  return intlMiddleware(req);
-});
+  // Extract the URL pathname from the request
+  const path = request.nextUrl.pathname;
+
+  // Allow direct access to sitemap.xml and robots.txt without i18n middleware processing
+  // This ensures these files are properly served for SEO purposes
+  // Related to GitHub issue: https://github.com/ixartz/Next-js-Boilerplate/issues/356
+  if (path === "/sitemap.xml" || path === "/robots.txt") {
+    return NextResponse.next();
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|monitoring|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
-    "/",
-    "/(en|ar)/:path*",
   ],
 };
