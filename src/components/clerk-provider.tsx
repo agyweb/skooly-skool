6263a491d840dd42b796d6b2b0ae24a72653999b
Abdellah M.El Idrissi <abdellahmoumenelidrissi@gmail.com>
2025-03-07 18:11:52 +0000

    feat: switch clerk's components language based on current local lang

--- src/components/clerk-provider.tsx
+++ src/components/clerk-provider.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { ClerkProvider as ImportedClerkProvider } from "@clerk/nextjs";
+import { arSA, enUS } from "@clerk/localizations";
+import { useLocale } from "next-intl";
 
 export default function ClerkProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ImportedClerkProvider>{children}</ImportedClerkProvider>;
+  const locale = useLocale();
+
+  return (
+    <ImportedClerkProvider localization={locale === "ar" ? arSA : enUS}>
+      {children}
+    </ImportedClerkProvider>
+  );
 }
