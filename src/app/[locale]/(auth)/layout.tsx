f2fb096d9128b2ae0b08b04072c5b2ae3625d6f6
Fadad Oussama <fadadoussama7@gmail.com>
2025-03-08 13:56:40 +0100

    feat: implement multi-language support for the auth layout

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 import { ClerkLoading } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { LoaderCircle } from "lucide-react";
+import { getTranslations } from "next-intl/server";
 
 export const metadata: Metadata = {
   title: "Skooly Skool | Authentication",
   description: "Continue to Skooly Skool by signing in or creating an account.",
 };
 
-export default function Authlayout({
+export default async function Authlayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const t = await getTranslations("footer");
+
   return (
     <div className="min-h-svh lg:flex">
       <div className="hidden flex-col justify-between border border-transparent bg-muted p-10 lg:flex lg:w-1/2">
@@ -27,10 +30,11 @@ export default function Authlayout({
         </div>
         <div>
           <p className="mb-4 text-[30px] font-medium">
-            Where Teachers and Students connect.
+            {t("logo-description")}.
           </p>
           <span className="text-xs font-light">
-            © {new Date().getFullYear()} Skooly Skool. All rights reserved.
+            <span className="font-geist">&copy;</span>
+            {new Date().getFullYear()} {t("rights")}
           </span>
         </div>
       </div>
